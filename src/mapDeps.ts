f895bbfef6952dab787b44df008146874e81e3cf
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-01-08 13:51:41 -0700

    feat: Adding better messages and update plans (#11)

--- src/mapDeps.ts
+++ src/mapDeps.ts
@@ -1,13 +1,10 @@
-import { dependencies } from './types.d';
+import chalk from 'chalk';
 import { parse } from 'parse-package-name';
-import FetchDeps from './tasks/FetchDeps';
 import semver from 'semver';
+import { nupUpdatePlan } from './banner';
+import FetchDeps from './tasks/FetchDeps';
 import FetchLatestVersion from './tasks/FetchLatestVersion';
-
-type pkg = {
-    name: string,
-    version: string
-}
+import { dependencies, pkg } from './types.d';
 
 const fetchLatestVersion = new FetchLatestVersion();
 const revDepMap: Record<string, { dependedBy: string[], level: number }> = {};
@@ -46,22 +43,28 @@ function populateRevDepMap(
 function assignLevels(pkgToFind: pkg, root: pkg) {
     let lvl = 1;
     let packages = new Set([...seeds]);
-    console.log(`\n\nNPM Update Plan for ${root.name}@${root.version} to make it compatible with ${pkgToFind.name}@${pkgToFind.version}\n\n`)
-    console.log(`Found ${packages.size} package(s), that are below required version = [${[...packages].join(', ')}])}`)
+    console.log(`Found ${packages.size} package(s), that are below required version [${[...packages].join(', ')}])}\n`)
+
+    nupUpdatePlan(pkgToFind, root);
+    // Assign levels to packages. Packages with level 0 are not updated.
     while (packages.size > 0) {
         packages = new Set([...packages].map(pkg => revDepMap[pkg].dependedBy).flat());
         packages.forEach(pkg => revDepMap[pkg].level = lvl);
         lvl += 1;
     }
-    const updateOrder = new Array(lvl).fill([]).map(() => new Array());
+
+    // Create update order.
+    const updateOrder = new Array(lvl - 2).fill([]).map(() => new Array());
     Object.entries(revDepMap).forEach(([pkg, { level }]) => {
-        updateOrder[level].push(pkg);
+        if (level > 0) { // Don't update packages with level 0.
+            updateOrder[level - 1].push(pkg);
+        }
     });
 
-    updateOrder[updateOrder.length - 1].push(`Finally Update ${root.name}@${root.version}`);
-
-    updateOrder.slice(1).forEach((pkgs, i) => {
-        console.log(`Phase ${i + 1}: \n- ${pkgs.join("\n- ")}\n`);
+    // dump update order in the terminal
+    updateOrder.forEach((pkgs, i) => {
+        console.log(chalk.bold.red(`Phase ${i + 1}:`));
+        console.log(`- [] ${pkgs.sort().join(`\n- [] `)}\n`);
     });
 }
 
